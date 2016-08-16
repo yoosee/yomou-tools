0db268031e3ca99c3dd45aaa183b5d21016bc161
Yoshinori Ikarashi <yoosee@gmail.com>
2016-08-16 11:34:04 -0400

    Follow changes in yomou.rb

--- yomou_merger.rb
+++ yomou_merger.rb
@@ -8,19 +8,30 @@ unless File.directory? bookdir
   exit
 end
 
-bookfile = "#{bookdir}/#{bookdir.gsub(/\//,'')}.txt"
+is_escape_ruby = false
+
+infofile = "#{bookdir}/info.txt"
 workdir = "#{bookdir}/work/"
 
-puts "merging file into #{bookfile}"
+info = Hash[ File.open(infofile).each_line.map {|l| l.chomp.split(":\s", 2) }]
+p info
 
+unless info['title'] && info['author']
+  puts "no title or author found."
+  exit
+end
+bookfile = "#{bookdir}/#{info['title']}\ \[#{info['author']}\].txt"
+
+puts "merging file into #{bookfile}"
 count_file = 0
 File.open(bookfile, 'w') do |book| 
-  book.puts bookdir
+  book.puts "#{info['title']} [#{info['author']}]"
   book.puts 
   Dir.open(workdir).grep(/\d+?\.txt/).sort{|a,b| a.gsub(/\.txt/,'').to_i <=> b.gsub(/\.txt/,'').to_i }.each do |file|
     File.open("#{workdir}/#{file}") do |f|
       f.each do |i|
-        book.write i.gsub(/&quot;/,'"').gsub(/《/,'<<').gsub(/》/,'>>').gsub(/&lt;/,'<').gsub(/&gt;/,'>').gsub(/&amp;/,'&')
+        i = i.gsub(/《/,'<<').gsub(/》/,'>>') if is_escape_ruby
+        book.write i.gsub(/&quot;/,'"').gsub(/&lt;/,'<').gsub(/&gt;/,'>').gsub(/&amp;/,'&')
       end
       book.puts "\n\n" 
     end
