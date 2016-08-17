8c91211b7157040e00326e6ff8cf1f99e343c5d7
Yoshinori Ikarashi <yoosee@gmail.com>
2016-08-17 09:45:48 -0400

    Adjustment for directory changes, timestamp management, etc.

--- yomou_batch.rb
+++ yomou_batch.rb
@@ -10,24 +10,28 @@ MARGE = 'yomou_merger.rb'
 BOOKLIST = 'booklist.txt'
 BOOKDIR  = 'books/'
 
-puts "Fetching books ===== #{Time.now}"
+puts "Fetching books start ===== #{Time.now}"
 File.open(BOOKLIST).each do |l|
   url, title = l.split(/ +/, 2)
   next unless /^http/ =~ url
   system "ruby #{YOMOU} #{url}"
-  sleep 30
+  sleep 20
 end
+puts "Fetching books end   ===== #{Time.now}"
 
-puts "Building books ===== #{Time.now}"
-Dir.open(".").each do |d|
-  next if ! File.directory? d or /^\./ =~ d
-  e = Shellwords.escape d
-  system "ruby #{MARGE} #{e}"
-  if ! File.exist?("#{BOOKDIR}/#{d}.txt") || 
-    (File.exist?("#{d}/#{d}.txt") && File.exist?("#{BOOKDIR}/#{d}.txt") && 
-     File.new("#{d}/#{d}.txt").size > File.new("#{BOOKDIR}/#{d}.txt").size)
-    puts "Updated: #{d}" 
-    FileUtils.mv "#{d}/#{d}.txt", BOOKDIR
+puts "Building books start ===== #{Time.now}"
+Dir.open("./work").each do |n|
+  d = "work/#{n}"
+  next if ! File.directory? d or /\/\./ =~ d
+  system "ruby #{MARGE} #{Shellwords.escape d}"
+  infofile = "#{d}/info.txt"
+  info = Hash[ File.open(infofile).each_line.map {|l| l.chomp.split(":\s", 2) }]
+  bookname = "#{info['title']}\ \[#{info['author']}\].txt"
+  if ! File.exist?("#{BOOKDIR}/#{bookname}") || 
+      (File.exist?("#{d}/#{bookname}") && 
+       File.new("#{d}/#{bookname}").size > File.new("#{BOOKDIR}/#{bookname}").size)
+    puts "Updated: #{bookname}" 
+    FileUtils.mv "#{d}/#{bookname}", BOOKDIR
   end
 end
-
+puts "Building books end ===== #{Time.now}"
