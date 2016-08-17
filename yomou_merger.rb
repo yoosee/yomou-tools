77230123a01345bc6cb840d8ffcfa1eee64c91b5
Yoshinori Ikarashi <yoosee@gmail.com>
2016-08-17 09:45:26 -0400

    Minor bugfix.

--- yomou_merger.rb
+++ yomou_merger.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/ruby
 # coding: utf-8
 
+require 'time'
+require 'fileutils'
 
 bookdir = ARGV.shift
 unless File.directory? bookdir
@@ -14,7 +16,6 @@ infofile = "#{bookdir}/info.txt"
 workdir = "#{bookdir}/work/"
 
 info = Hash[ File.open(infofile).each_line.map {|l| l.chomp.split(":\s", 2) }]
-p info
 
 unless info['title'] && info['author']
   puts "no title or author found."
@@ -22,8 +23,9 @@ unless info['title'] && info['author']
 end
 bookfile = "#{bookdir}/#{info['title']}\ \[#{info['author']}\].txt"
 
-puts "merging file into #{bookfile}"
+puts "merging files into #{bookfile}"
 count_file = 0
+line = 0
 File.open(bookfile, 'w') do |book| 
   book.puts "#{info['title']} [#{info['author']}]"
   book.puts 
@@ -32,6 +34,7 @@ File.open(bookfile, 'w') do |book|
       f.each do |i|
         i = i.gsub(/《/,'<<').gsub(/》/,'>>') if is_escape_ruby
         book.write i.gsub(/&quot;/,'"').gsub(/&lt;/,'<').gsub(/&gt;/,'>').gsub(/&amp;/,'&')
+        line += 1
       end
       book.puts "\n\n" 
     end
@@ -39,5 +42,13 @@ File.open(bookfile, 'w') do |book|
   end
 end
 
-puts "#{count_file} files merged."
+def format_num num
+  num.to_s.gsub(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1,")
+end
+
+lastupdated_time = Time.parse("#{info['last_updated']} +0900")
+FileUtils.touch bookfile, :mtime => lastupdated_time
+
+size = File.open(bookfile).size / 1024
+puts "#{count_file} files merged. total #{format_num(line+2)} lines, #{format_num(size)} KB. updated #{lastupdated_time.to_s}"
 
